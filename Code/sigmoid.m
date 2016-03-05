35c4ba6bf8b759a9fd169977cb0957833e52cc93
Chamarthi Venkata Hrudayanath <hrudaynathraju@gmail.com>
2016-03-06 01:30:25 +0530

    Update sigmoid.m

--- Code/sigmoid.m
+++ Code/sigmoid.m
@@ -1,5 +1,6 @@
 function si = sigmoid( input , derivative )
 % Sigmoid function calculation 
+% if derivative is 1 then it returns sigmoid derivative value else if 0 then it simply returns sigmoid value.
 
 val = 1.0/(1.0+ exp(-input));
 if derivative == 1
